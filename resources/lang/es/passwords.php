4f9a21efa51c6506d365c48629ffaeac2caf455b
giovanny <giovanny@messoft.com>
2019-02-25 17:39:24 -0600

    translations

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña Passwords debe ser de al menos seis caracteres y coincidir con la confirmación.',
+    'reset' => 'Tu contraseña ha sido reestablecida!',
+    'sent' => 'Te hemos enviado un correo electrónico con el link para restablecer tu contraseña!',
+    'token' => 'Este token para restablecer contraseña es inválido.',
+    'user' => "No podemos encontrar un usuario con esa dirección de correo.",
+
+];

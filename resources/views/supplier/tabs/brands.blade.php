b3e9f11f5e7dc75db5dee2d6c0c0c1e41e829a7d
giovanny <giovanny@messoft.com>
2019-03-04 11:43:49 -0600

    adding brands html

--- resources/views/supplier/tabs/brands.blade.php
+++ resources/views/supplier/tabs/brands.blade.php
@@ -1,10 +1,211 @@
 <div class="form-body">
     <div class="row">
-        dasd
+    	<div class="col-md-4"></div>
+	    <div class="col-md-3">
+	        <label for="brands_select2" class="control-label">Marca</label>
+	        <select id="brands_select2" class="form-control input-sm select2-multiple" style="width: 100%">
+	                <option value="AK">Alaska</option>
+	                <option value="CA">California</option>
+	                <option value="NV">Nevada</option>
+	                <option value="OR">Oregon</option>
+	                <option value="WA">Washington</option>
+	                <option value="AZ">Arizona</option>
+	                <option value="CO">Colorado</option>
+	                <option value="ID">Idaho</option>
+	                <option value="MT">Montana</option>
+	                <option value="NE">Nebraska</option>
+	                <option value="NM">New Mexico</option>
+	                <option value="ND">North Dakota</option>
+	                <option value="UT">Utah</option>
+	                <option value="WY">Wyoming</option>
+	                <option value="AL">Alabama</option>
+	                <option value="AR">Arkansas</option>
+	                <option value="IL">Illinois</option>
+	                <option value="IA">Iowa</option>
+	                <option value="KS">Kansas</option>
+	                <option value="KY">Kentucky</option>
+	                <option value="LA">Louisiana</option>
+	                <option value="MN">Minnesota</option>
+	                <option value="MS">Mississippi</option>
+	                <option value="MO">Missouri</option>
+	                <option value="OK">Oklahoma</option>
+	                <option value="SD">South Dakota</option>
+	                <option value="TX">Texas</option>
+	                <option value="TN">Tennessee</option>
+	                <option value="WI">Wisconsin</option>
+	                <option value="CT">Connecticut</option>
+	                <option value="DE">Delaware</option>
+	                <option value="FL">Florida</option>
+	                <option value="GA">Georgia</option>
+	                <option value="IN">Indiana</option>
+	                <option value="ME">Maine</option>
+	                <option value="MD">Maryland</option>
+	                <option value="MA">Massachusetts</option>
+	                <option value="MI">Michigan</option>
+	                <option value="NH">New Hampshire</option>
+	                <option value="NJ">New Jersey</option>
+	                <option value="NY">New York</option>
+	                <option value="NC">North Carolina</option>
+	                <option value="OH">Ohio</option>
+	                <option value="PA">Pennsylvania</option>
+	                <option value="RI">Rhode Island</option>
+	                <option value="SC">South Carolina</option>
+	                <option value="VT">Vermont</option>
+	                <option value="VA">Virginia</option>
+	                <option value="WV">West Virginia</option>
+	        </select>
+	    </div>
+	    <div class="col-md-1">
+	        <button type="button" class="btn green" style="margin-top: 25px">
+				<i class="fa fa-plus"></i> Agregar</button>
+		</div>
+		<div class="col-md-4"></div>
+    </div>
+    <div class="row" style="margin-top: 20px">
+    	<div class="col-md-4"></div>
+    	<div class="col-md-4">
+		    <table class="table table-striped table-hover table-bordered" id="brands_table">
+				<thead>
+				    <tr>
+				        <th>Name</th>
+				        <th></th>
+				    </tr>
+				</thead>
+				<tbody>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> lisa </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> lisa </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> lisa </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> lisa </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> nick12 </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> goldweb </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> webriver </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> gist124 </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				    <tr>
+				        <td> alex </td>
+				        <td>
+				            <a class="delete" href="javascript:;"> Delete </a>
+				        </td>
+				    </tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="col-md-4"></div>
     </div>
 </div>
 <div class="form-actions right">
     <button type="button" class="btn default">Cancelar</button>
     <button type="button" class="btn blue">
-        <i class="fa fa-check"></i> Continuar</button>
+        <i class="fa fa-check"></i> Guardar</button>
 </div>

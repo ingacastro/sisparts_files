bcb3529f8d5412b84105d5884c5f50c4b9770d0e
giovanny <giovanny@messoft.com>
2019-07-04 16:01:28 -0500

    customer update command, done

--- app/Customer.php
+++ app/Customer.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-	protected $primaryKey = 'users_id';
+	//protected $primaryKey = 'users_id';
     protected $fillable = ['code', 'trade_name', 'business_name', 'post_code', 'state', 'country', 'sync_connections_id'];
 }

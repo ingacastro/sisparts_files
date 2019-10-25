099a2aff38ac3485b298f4cad54b3ecf1c1dd654
giovanny <giovanny@messoft.com>
2019-10-25 15:56:57 -0500

    Attaching manufacturers to suppliers on virtual_catalog migration

--- app/Console/Commands/VirtualCatalogSync.php
+++ app/Console/Commands/VirtualCatalogSync.php
@@ -12,6 +12,7 @@ use IParts\File;
 use IParts\SyncConnection;
 use IParts\Replacement;
 use IParts\Observation;
+use IParts\Manufacturer;
 use DB;
 use Storage;
 
@@ -38,8 +39,8 @@ class VirtualCatalogSync extends Command
      */
     public function __construct()
     {
-        parent::__construct();
-        $this->virtual_cat_conn_name = 'mysql_virtual_catalog';
+      parent::__construct();
+      $this->virtual_cat_conn_name = 'mysql_virtual_catalog';
     }
 
     /**
@@ -49,35 +50,35 @@ class VirtualCatalogSync extends Command
      */
     public function handle()
     {
-        $this->main();
+      $this->main();
     }
 
     private function main()
     {
-        $virtual_cat_sets = DB::connection($this->virtual_cat_conn_name)->table('cotizacion')
-        ->get(); //->where('rfq', '!=', null)
+      $virtual_cat_sets = DB::connection($this->virtual_cat_conn_name)
+        ->table('cotizacion')->get(); //->where('rfq', '!=', null)
 
         foreach($virtual_cat_sets as $virtual_cat_set) {
-            DB::transaction(function() use ($virtual_cat_set) {            
-                $document = $this->createDocument($virtual_cat_set);
-                $this->createDocumentSupply($virtual_cat_set, $document);
-            });
+          DB::transaction(function() use ($virtual_cat_set) {            
+            $document = $this->createDocument($virtual_cat_set);
+            $this->createDocumentSupply($virtual_cat_set, $document);
+          });
         }
-    }
+      }
 
-    private function createDocument(\stdClass $virtual_cat_set)
-    {
+      private function createDocument(\stdClass $virtual_cat_set)
+      {
         $sync_connection = SyncConnection::where('name', 'mysql_catalogo_virtual')->first();
         return Document::create([
-            'sync_connections_id' => $sync_connection->id,
-            'reference' => $virtual_cat_set->rfq,
-            'type' => 'PCT',
-            'status' => 1
+          'sync_connections_id' => $sync_connection->id,
+          'reference' => $virtual_cat_set->rfq,
+          'type' => 'PCT',
+          'status' => 1
         ]);
-    }
+      }
 
-    private function createDocumentSupply(\stdClass $virtual_cat_set, Document $document)
-    {
+      private function createDocumentSupply(\stdClass $virtual_cat_set, Document $document)
+      {
         $proveedor = $virtual_cat_set->proveedor;
         
         $supply = $this->createSupply($proveedor, $virtual_cat_set->num_parte);
@@ -85,92 +86,111 @@ class VirtualCatalogSync extends Command
         $suppliers_id = null;
 
         if(!empty($proveedor)) {
-             $supplier = Supplier::where('trade_name', $proveedor)->first() ?? 
-             Supplier::create(['trade_name' => $proveedor, 'marketplace' => 0]);
-             $suppliers_id = $supplier->id;
-         }
-         
-        SupplySet::create([
-            'documents_id' => $document->id,
-            'supplies_id' => $supply->id,
+         $supplier = Supplier::where('trade_name', $proveedor)->first() ?? 
+         Supplier::create(['trade_name' => $proveedor, 'marketplace' => 0]);
+         $this->attachSupplierManufacturers($supplier);
+         $suppliers_id = $supplier->id;
+       }
+
+       SupplySet::create([
+        'documents_id' => $document->id,
+        'supplies_id' => $supply->id,
             'status' => 1, //Solicitud de cotización no enviada
             'suppliers_id' => $suppliers_id
-        ]);
+          ]);
+     }
+
+     private function attachSupplierManufacturers(Supplier $supplier)
+     {
+      $siavcom_manufacturers = DB::connection($this->virtual_cat_conn_name)->table('producto_proveedor')
+      ->where('proveedor', $supplier->trade_name)
+      ->whereNotNull('marca')
+      ->where('marca', '!=', '')->get();
+
+      $manufacturers_ids = [];
+      $supplier_brands_ids = $supplier->brands->pluck('id')->toArray();
+      foreach ($siavcom_manufacturers as $siavcom_manufacturer) {
+        $sisparts_manufacturer = Manufacturer::where('name', trim($siavcom_manufacturer->marca))->first() ?? 
+                                 Manufacturer::create(['name' => $siavcom_manufacturer->marca]);
+        if(!in_array($sisparts_manufacturer->id, $supplier_brands_ids)) 
+          $manufacturers_ids[] = $sisparts_manufacturer->id;
+      }
+      $supplier->brands()->attach(array_unique($manufacturers_ids));
     }
 
     private function createSupply($proveedor, $number)
     {
-        $supply = Supply::where('number', $number)->first();
+      $supply = Supply::where('number', $number)->first();
 
-        if($supply == null)
-            $supply = Supply::create([
-                'number' => $number,
+      if($supply == null)
+        $supply = Supply::create([
+          'number' => $number,
                 'tax' => 2 //Empty tax
-            ]);
+              ]);
 
-        $this->createFiles($supply, $number, $proveedor);
-        $this->createObservations($supply);
-        $this->createReplacements($supply);
+      $this->createFiles($supply, $number, $proveedor);
+      $this->createObservations($supply);
+      $this->createReplacements($supply);
 
-        return $supply;
+      return $supply;
     }
 
     /*creates file if any, otherwise looks for a url */
     private function createFiles(Supply $supply, $supply_number, $proveedor)
     {
-        $virtual_cat_files = DB::connection($this->virtual_cat_conn_name)->table('archivos_num_parte')
-        ->where('num_parte', $supply_number)
-        ->get();
+      $virtual_cat_files = DB::connection($this->virtual_cat_conn_name)->table('archivos_num_parte')
+      ->where('num_parte', $supply_number)
+      ->get();
 
-        $virtual_catalog_urls = DB::connection($this->virtual_cat_conn_name)->table('producto_url')
-        ->where('num_parte', $supply_number)
-        ->get();
+      $virtual_catalog_urls = DB::connection($this->virtual_cat_conn_name)->table('producto_url')
+      ->where('num_parte', $supply_number)
+      ->get();
 
-        $file_name = '';
-        $supply_id = $supply->id;
+      $file_name = '';
+      $supply_id = $supply->id;
 
-        foreach($virtual_cat_files as $virtual_cat_file) {
-            try {
-                $file_name = uniqid() . '_' . $virtual_cat_file->nombre;
-                Storage::disk('supplies_files')->put($file_name, $virtual_cat_file->contenido);
+      foreach($virtual_cat_files as $virtual_cat_file) {
+        try {
+          $file_name = uniqid() . '_' . $virtual_cat_file->nombre;
+          Storage::disk('supplies_files')->put($file_name, $virtual_cat_file->contenido);
 
-                $data['supplier'] = $proveedor;
-                $data['path'] = 'storage/supplies_files/' . $file_name;
+          $data['supplier'] = $proveedor;
+          $data['path'] = 'storage/supplies_files/' . $file_name;
 
-                $file = File::create($data);
-                $file->supplies()->attach([$supply_id]);
+          $file = File::create($data);
+          $file->supplies()->attach([$supply_id]);
 
-            } catch(\Exception $e) {
-                Storage::disk('supplies_files')->delete($file_name);
-                Log::notice($e->getMessage());
-            }
+        } catch(\Exception $e) {
+          Storage::disk('supplies_files')->delete($file_name);
+          Log::notice($e->getMessage());
         }
+      }
 
-        foreach($virtual_catalog_urls as $virtual_catalog_url) {
-            $url_data['supplier'] = $proveedor;
-            $url_data['url'] = $virtual_catalog_url->url;
+      foreach($virtual_catalog_urls as $virtual_catalog_url) {
+        $url_data['supplier'] = $proveedor;
+        $url_data['url'] = $virtual_catalog_url->url;
 
-            $file = File::create($url_data);
-            $file->supplies()->attach([$supply_id]);
-        }
+        $file = File::create($url_data);
+        $file->supplies()->attach([$supply_id]);
+      }
     }
 
     private function createObservations(Supply $supply)
     {
-        $observations = DB::connection($this->virtual_cat_conn_name)->table('producto_observaciones')
-        ->where('num_parte', $supply->number)->get();
+      $observations = DB::connection($this->virtual_cat_conn_name)->table('producto_observaciones')
+      ->where('num_parte', $supply->number)->get();
 
-        foreach($observations as $observation) {
-            Observation::create(['supplies_id' => $supply->id, 'description' => $observation->observacion]);
-        }
+      foreach($observations as $observation) {
+        Observation::create(['supplies_id' => $supply->id, 'description' => $observation->observacion]);
+      }
     }
     private function createReplacements(Supply $supply)
     {
-        $replacements = DB::connection($this->virtual_cat_conn_name)->table('producto_reemplazo')
-        ->where('num_parte', $supply->number)->get();
+      $replacements = DB::connection($this->virtual_cat_conn_name)->table('producto_reemplazo')
+      ->where('num_parte', $supply->number)->get();
 
-        foreach($replacements as $replacement) {
-            Replacement::create(['supplies_id' => $supply->id, 'description' => $replacement->reemplazo]); 
-        }       
+      foreach($replacements as $replacement) {
+        Replacement::create(['supplies_id' => $supply->id, 'description' => $replacement->reemplazo]); 
+      }       
     }
-}
+  }
